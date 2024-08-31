5bc1209d60081730c644e7eb1bb92354275c528c
Venu Poruri <vmporuri@gmail.com>
2024-08-31 12:30:11 -0700

    refactor: move textscreen class into header file

--- src/game/text-screen.h
+++ src/game/text-screen.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "game-state.h"
+#include "raylib.h"
 #include <string>
 
 class TextScreen : public GameState {
@@ -11,7 +12,12 @@ class TextScreen : public GameState {
     int m_fontSize;
 
   public:
-    TextScreen(int fontSize = s_defaultFontSize);
-    TextScreen(const char *text, int fontSize = s_defaultFontSize);
-    void render() override;
+    TextScreen(int fontSize) : m_fontSize { fontSize } {}
+    TextScreen(const char *text, int fontSize)
+        : m_text { text }, m_fontSize { fontSize } {}
+    void render() override {
+        int textWidth { MeasureText(m_text.c_str(), m_fontSize) };
+        DrawText(m_text.c_str(), (GetScreenWidth() - textWidth) / 2,
+                 GetScreenHeight() / 2, m_fontSize, WHITE);
+    }
 };
