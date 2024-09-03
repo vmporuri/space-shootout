e3b8e7228b1109936f2fb5cc1eabecbc332f73b6
Venu Poruri <vmporuri@gmail.com>
2024-09-02 22:48:04 -0700

    feat: add game messages for peer communication

---
+++ src/game/game-message.cpp
@@ -0,0 +1,34 @@
+#include "game-message.h"
+#include "nlohmann/json.hpp"
+
+GameMessage::GameMessage(MessageType type, std::vector<int> &lasers)
+    : m_type { type }, m_lasers { lasers } {}
+
+[[nodiscard]] GameMessage::MessageType GameMessage::getMessageType() const {
+    return m_type;
+}
+
+[[nodiscard]] std::vector<int> GameMessage::getLasers() const {
+    return m_lasers;
+}
+
+[[nodiscard]] std::string GameMessage::marshall() const {
+    nlohmann::json msgJSON;
+    msgJSON[s_messageField] = (int)m_type;
+    msgJSON[s_incomingField] = m_lasers;
+    return msgJSON.dump();
+}
+
+void GameMessage::unmarshall(std::string msg) {
+    nlohmann::json msgJSON { nlohmann::json::parse(msg) };
+
+    if (msgJSON.contains(s_messageField) &&
+        msgJSON[s_messageField].is_number_integer()) {
+        m_type = msgJSON[s_messageField];
+    }
+
+    if (msgJSON.contains(s_incomingField) &&
+        msgJSON[s_incomingField].is_array()) {
+        m_lasers = msgJSON[s_incomingField].get<std::vector<int>>();
+    }
+}
---
+++ src/game/game-message.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class GameMessage {
+
+  public:
+    enum MessageType { INCOMING_LASERS = 0, DECLARE_LOSS = 1 };
+
+  private:
+    constexpr static auto s_messageField { "messageType" };
+    constexpr static auto s_incomingField { "incomingLasers" };
+
+    MessageType m_type;
+    std::vector<int> m_lasers;
+
+  public:
+    GameMessage(MessageType type, std::vector<int> &lasers);
+    [[nodiscard]] MessageType getMessageType() const;
+    [[nodiscard]] std::vector<int> getLasers() const;
+    [[nodiscard]] std::string marshall() const;
+    void unmarshall(std::string msg);
+};
